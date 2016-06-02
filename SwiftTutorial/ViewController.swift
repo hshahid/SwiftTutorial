25f41e8799ddc529197b8fed57bba8145100378b
Hashma <shahi22h@mtholyoke.edu>
2016-06-02 09:35:54 +0500

    Initial Commit

---
+++ SwiftTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTutorial
+//
+//  Created by Hashma Shahid on 6/2/16.
+//  Copyright (c) 2016 Hashma Shahid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
