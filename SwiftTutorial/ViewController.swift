dbd624d391b4a9fa5d4fdb2faecbafde448a6f80
Hashma <shahi22h@mtholyoke.edu>
2016-06-03 11:04:59 +0500

    “update”

--- SwiftTutorial/ViewController.swift
+++ SwiftTutorial/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
 
+    @IBOutlet weak var NameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
