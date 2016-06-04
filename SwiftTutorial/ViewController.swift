7c951e46dbb065f24fe64c9dde707500bf15529b
Hashma <shahi22h@mtholyoke.edu>
2016-06-04 22:08:18 +0500

    “update”

--- SwiftTutorial/ViewController.swift
+++ SwiftTutorial/ViewController.swift
@@ -29,6 +29,10 @@ class ViewController: UIViewController,  UITextFieldDelegate {
         return true
     }
     
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
     //MARK: Actions
 
     @IBAction func setDefaultLabelText(sender: UIButton) {
